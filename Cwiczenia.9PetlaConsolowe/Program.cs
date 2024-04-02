672cc782cd764687e8ce7fb528d24802f6053d13
Wojciech Zawadzki <zawadzki1993@hotmail.com>
2024-04-02 20:32:31 +0200

    Add project files.

---
+++ Cwiczenia.9PetlaConsolowe/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class DecimalToBinaryConverter
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Podaj liczbę dziesiętną do przekonwertowania na binarną:");
+		int decimalNumber = Convert.ToInt32(Console.ReadLine());
+
+		string binaryNumber = DecimalToBinary(decimalNumber);
+
+		Console.WriteLine($"Liczba binarna dla {decimalNumber} to: {binaryNumber}");
+	}
+
+	static string DecimalToBinary(int decimalNumber)
+	{
+		string binary = "";
+
+		while (decimalNumber > 0)
+		{
+			int remainder = decimalNumber % 2;
+			binary = remainder + binary;
+			decimalNumber /= 2;
+		}
+
+		return binary;
+	}
+}
